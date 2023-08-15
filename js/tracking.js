a0fc2e3363cee603f90badedb7ed7d37c4fd8f2f
ch1on <jcw0337@gmail.com>
2023-08-16 02:44:24 +0900

    0816_정채원_장바구니,상세페이지,배송조회_수정2

--- js/tracking.js
+++ js/tracking.js
@@ -1,13 +1,37 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
+document.addEventListener("DOMContentLoaded", function() {
+  //드롭다운에 화살표 이미지랑 첫번째 요소 같이 보이도록
+  var dropdownContent1 = document.getElementById("myDropdown1");
+  var firstItem1 = dropdownContent1.querySelector("a:first-child");
+  var dropbtn1 = document.querySelector(".dropbtn");
+  var image1 = dropbtn1.querySelector("img");
+
+
+  dropbtn1.innerHTML = ""; // Clear the existing content
+  dropbtn1.appendChild(firstItem1.cloneNode(true));
+  dropbtn1.appendChild(image1);
+
+  var dropdownContent2 = document.getElementById("myDropdown2");
+  var firstItem2 = dropdownContent2.querySelector("a:first-child");
+  var dropbtn2 = document.getElementById("dropbtn2"); // Use the ID of the second dropbtn
+
+  var image2 = dropbtn2.querySelector("img");
+  dropbtn2.innerHTML = ""; // Clear the existing content
+  dropbtn2.appendChild(firstItem2.cloneNode(true));
+  dropbtn2.appendChild(image2);
+
+
+});
+
+/* 토글 보이도록*/
 function myFunction(event) {
     event.preventDefault();
     document.getElementById("myDropdown1").classList.toggle("show");
 }
-function myFunction2() {
+function myFunction2(event) {
     event.preventDefault();
     document.getElementById("myDropdown2").classList.toggle("show");
 }
+
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
